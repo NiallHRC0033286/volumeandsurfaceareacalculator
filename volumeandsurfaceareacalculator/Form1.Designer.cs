b9b73eae546c1d0733a4db4ccb61ebbf2c8fa55b
rc0033286 <rc0033286@CRO-24-10>
2019-03-18 15:05:14 +0000

    Add project files.

---
+++ volumeandsurfaceareacalculator/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace volumeandsurfaceareacalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ShapeSelect = new System.Windows.Forms.ComboBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(407, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(359, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Volume";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(407, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Surface Area";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ShapeSelect
+            // 
+            this.ShapeSelect.FormattingEnabled = true;
+            this.ShapeSelect.Items.AddRange(new object[] {
+            "Sphere",
+            "Cone",
+            "Cylinder",
+            "Cube",
+            "Cuboid"});
+            this.ShapeSelect.Location = new System.Drawing.Point(187, 39);
+            this.ShapeSelect.Name = "ShapeSelect";
+            this.ShapeSelect.Size = new System.Drawing.Size(121, 21);
+            this.ShapeSelect.TabIndex = 7;
+            this.ShapeSelect.Text = "Select Shape";
+            this.ShapeSelect.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(71, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(71, 93);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Length";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Width";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(71, 120);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Height";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 208);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.ShapeSelect);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox ShapeSelect;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ volumeandsurfaceareacalculator/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volumeandsurfaceareacalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ShapeSelect.Text == "Sphere")
+                {
+                    double radius = double.Parse(textBox1.Text);
+                    double V = 1.3333 * 3.14159265359 * (radius * radius * radius);
+                    textBox2.ResetText();
+                    textBox2.AppendText(V.ToString());
+                    double A = 4 * 3.14159265359 * (radius * radius);
+                    textBox3.ResetText();
+                    textBox3.AppendText(A.ToString());
+                }
+                if (ShapeSelect.Text == "Cone")
+                {
+                    double radius = double.Parse(textBox1.Text);
+                    double H = double.Parse(textBox4.Text);
+                    double V = 3.14159265359 * (radius * radius) * (H / 3);
+                    textBox2.ResetText();
+                    textBox2.AppendText(V.ToString());
+                    double A = 3.14159265359 * radius * (radius + Math.Sqrt((H * H) + (radius * radius)));
+                    textBox3.ResetText();
+                    textBox3.AppendText(A.ToString());
+                }
+                if (ShapeSelect.Text == "Cylinder")
+                {
+                    double radius = double.Parse(textBox1.Text);
+                    double H = double.Parse(textBox4.Text);
+                    double V = 3.14159265359 * (radius * radius) * H;
+                    textBox2.ResetText();
+                    textBox2.AppendText(V.ToString());
+                    double A = (2 * (3.14159265359 * (radius * radius))) + ((3.14159265359 * radius * 2) * H);
+                    textBox3.ResetText();
+                    textBox3.AppendText(A.ToString());
+                }
+                if (ShapeSelect.Text == "Cube")
+                {
+                    double H = double.Parse(textBox4.Text);
+                    double V = H * H * H;
+                    textBox2.ResetText();
+                    textBox2.AppendText(V.ToString());
+                    double A = H * H * 6;
+                    textBox3.ResetText();
+                    textBox3.AppendText(A.ToString());
+                }
+                if (ShapeSelect.Text == "Cuboid")
+                {
+                    double H = double.Parse(textBox4.Text);
+                    double W = double.Parse(textBox5.Text);
+                    double L = double.Parse(textBox6.Text);
+                    double V = H * W * L;
+                    textBox2.ResetText();
+                    textBox2.AppendText(V.ToString());
+                    double A = (H * W * 2) + (W * L * 2) + (L * H * 2);
+                    textBox3.ResetText();
+                    textBox3.AppendText(A.ToString());
+                }
+            }
+            catch
+            {
+                textBox2.Text = "Error";
+                textBox3.Text = "Error";
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ShapeSelect.Text == "Sphere") 
+            {
+                textBox1.ReadOnly = false;
+                textBox4.ReadOnly = true;
+                textBox5.ReadOnly = true;
+                textBox6.ReadOnly = true;
+            }
+            if (ShapeSelect.Text == "Cone") 
+            {
+                textBox1.ReadOnly = false;
+                textBox4.ReadOnly = false;
+                textBox5.ReadOnly = true;
+                textBox6.ReadOnly = true;
+            }
+            if (ShapeSelect.Text == "Cylinder") 
+            {
+                textBox1.ReadOnly = false;
+                textBox4.ReadOnly = false;
+                textBox5.ReadOnly = true;
+                textBox6.ReadOnly = true;
+            }
+            if (ShapeSelect.Text == "Cube") 
+            {
+                textBox1.ReadOnly = true;
+                textBox4.ReadOnly = false;
+                textBox5.ReadOnly = true;
+                textBox6.ReadOnly = true;
+            }
+            if (ShapeSelect.Text == "Cuboid") 
+            {
+                textBox1.ReadOnly = true;
+                textBox4.ReadOnly = false;
+                textBox5.ReadOnly = false;
+                textBox6.ReadOnly = false;
+            }
+        }
+    }
+}
